0e40e9d1d3038877b238c9eb7972207862fcff63
abjudge <abdelrahmanelkady0@gmail.com>
2023-06-18 13:55:52 +0300

    added new models and req.txt file

--- backend/pms/settings.py
+++ backend/pms/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'djoser',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,8 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'djoser',
     'accounts',
+    'workspace',
+    'project',
+    'task',
+    'meeting',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +61,7 @@ ROOT_URLCONF = 'pms.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'frontend','build')],
+        'DIRS': [os.path.join(BASE_DIR,'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,7 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'frontend','build','static')]
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'build','static')]
 
 STATIC_ROOT=os.path.join(BASE_DIR,'static')
 
