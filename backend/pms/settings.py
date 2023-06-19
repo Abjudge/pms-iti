e6a7817400d5b63f156d1b422215c40f2c38ec32
abjudge <abdelrahmanelkady0@gmail.com>
2023-06-19 11:37:34 +0300

    added crud apis for meeting

--- backend/pms/settings.py
+++ backend/pms/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -146,6 +147,8 @@ REST_FRAMEWORK={
     }
 SIMPLE_JWT={
     'AUTH_HEADER_TYPES':('JWT',),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=600),
+
     }
 
 AUTH_USER_MODEL='accounts.UserAccount'
--- backend/pms/urls.py
+++ backend/pms/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/',include('djoser.urls')),
     path('auth/',include('djoser.urls.jwt')),
-    
+    # path('workspace/',include('workspace.urls')),
+    # path('project/',include('project.urls')),
+    # path('task/',include('task.urls')),
+    path('meeting/',include('meeting.urls')),
 ]
 urlpatterns += [re_path(r'^.*',TemplateView.as_view(template_name='index.html'))]
