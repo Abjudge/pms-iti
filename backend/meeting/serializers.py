c99dca0ae5f074507e8d17906bec96315be7cbe7
abdelrahman elkady <112387841+Abjudge@users.noreply.github.com>
2023-06-19 19:12:32 +0300

    Revert "Workspace apis"

--- backend/meeting/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import *
-from django.db.models import fields
-
-class MeetingSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Meeting
-        fields = '__all__'
